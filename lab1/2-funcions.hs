34b4f7529161eb2cee45e8d89c581e0ccbc0b98d
Elies <68759092+alias313@users.noreply.github.com>
2025-09-15 17:18:09 +0200

    Versió en lambda

--- lab1/2-funcions.hs
+++ lab1/2-funcions.hs
@@ -4,3 +4,8 @@ primer x y = x
 segon :: Double -> Double -> Double
 segon x y = y
 
+primerL :: Double -> Double -> Double
+primerL = \x -> \y -> x
+
+segonL :: Double -> Double -> Double
+segonL = \x -> \y -> y
