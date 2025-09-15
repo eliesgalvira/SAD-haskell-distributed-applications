13a29a4a4335c1cea5277c0ffed7212aeb87e11f
Elies <68759092+alias313@users.noreply.github.com>
2025-09-15 23:01:52 +0200

    Selector

--- lab1/3-tipus.hs
+++ lab1/3-tipus.hs
@@ -1,1 +1,14 @@
 data M3 = A | B | C
+
+
+cert :: a -> b -> a
+cert x _ = x
+
+fals :: a -> b -> b
+fals _ y = y
+
+(.<.) :: M3 -> M3 -> (a -> a -> a)
+A .<. B = cert
+A .<. C = cert
+B .<. C = cert
+_ .<. _ = fals
