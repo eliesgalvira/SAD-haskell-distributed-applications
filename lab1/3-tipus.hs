1b901423a0b4ad32c18a0a5519a410e187453302
Elies <68759092+alias313@users.noreply.github.com>
2025-09-15 23:14:58 +0200

    Lab1 acabat

--- lab1/3-tipus.hs
+++ lab1/3-tipus.hs
@@ -1,4 +1,5 @@
 data M3 = A | B | C
+  deriving(Show)
 
 
 cert :: a -> b -> a
@@ -12,3 +13,23 @@ A .<. B = cert
 A .<. C = cert
 B .<. C = cert
 _ .<. _ = fals
+
+
+max2 :: M3 -> M3 -> M3
+max2 x y = (x .<. y) y x
+
+maxim :: M3 -> M3 -> M3 -> M3
+maxim a b c = max2 (max2 a b) c
+
+maximDup :: M3 -> M3 -> M3 -> M3
+maximDup a b c = ((max2 a b) .<. c) c (max2 a b)
+
+maximLet :: M3 -> M3 -> M3 -> M3
+maximLet a b c =
+  let ab = max2 a b
+  in (ab .<. c) c ab
+
+maximWhere :: M3 -> M3 -> M3 -> M3
+maximWhere a b c = (ab .<. c) c ab
+  where
+    ab = max2 a b
