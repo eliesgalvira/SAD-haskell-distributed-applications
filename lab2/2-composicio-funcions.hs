573ca29dd4e759a58c30add8d9515bd7237294bb
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-10-20 15:06:51 +0200

    The indentation debacle

--- lab2/2-composicio-funcions.hs
+++ lab2/2-composicio-funcions.hs
@@ -1,10 +1,28 @@
 -- Exercici: definir les funcions (.) i ($)
-(.$.) :: (b -> c) -> (a -> b) -> a -> c
-(.$.) f g = \x -> f (g x)
+(...) :: (b -> c) -> (a -> b) -> a -> c
+(...) f g = \x -> f (g x)
 
-(...) :: (a -> b) -> a -> b
-(...) f = f
+(.$.) :: (a -> b) -> a -> b
+(.$.) f x = f x
+
+-- ghci> :t 2.43523 * 10 ^ 3
+-- 2.43523 * 10 ^ 3 :: Fractional a => a
+-- ghci> :t round (2.43523 * 10 ^ 3
+-- round (2.43523 * 10 ^ 3) :: Integral b => b
+-- ghci> :t fromIntegral . round $ 2.43523 * 10 ^ 3
+-- fromIntegral . round $ 2.43523 * 10 ^ 3 :: Num c => c
 
 -- Exercici: definir la funció arrodonir
 arrodonir :: Double -> Integer -> Double
-arrodonir valor numeroDecimals = (fromIntegral . round $ valor * 10 ^ numeroDecimals) / 10 ^ numeroDecimals
+arrodonir valor numeroDecimals = (fromIntegral ... round .$. (valor * 10 ^ numeroDecimals)) / 10 ^ numeroDecimals
+
+arrodonirLet :: Double -> Integer -> Double
+arrodonirLet valor numeroDecimals =
+  let
+    zerosDecimals = 10 ^ numeroDecimals
+    arrodonirADouble = (...) fromIntegral round
+  in arrodonirADouble .$. (valor * zerosDecimals) / zerosDecimals
+
+-- Exercici: definir la funció arrodonirb
+arrodonirb :: Double -> Integer -> Double
+arrodonirb valor numeroDecimals = (fromIntegral . round $ valor * 10 ^ numeroDecimals) / 10 ^ numeroDecimals
