254d3e6981f094c06d2b65d8d7a450e8cc75724e
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-10-26 10:19:07 +0100

    f1 feta

--- lab2/2-composicio-funcions.hs
+++ lab2/2-composicio-funcions.hs
@@ -32,19 +32,26 @@ arrodonirb valor numeroDecimals = (fromIntegral . round $ valor * 10 ^ numeroDec
 -- 1. Definir la funció elevar al quadrat
 quad :: Int -> Int
 quad x = x * x
-
 -- 2. Definir la funció multiplicar
 multiplicar :: Int -> Int -> Int
 multiplicar x y = x * y
-
 -- 3. Definir la funció triple
 triple :: Int -> Int
 triple x = x * 3
-
 -- 4. Definir la funció triplequad (3n)^2
 triplequad :: Int -> Int
 triplequad = quad . triple
-
 -- 5. Definir la funció multiplicarquad (n*m)^2
 multiplicarquad :: Int -> Int -> Int
 multiplicarquad m n = quad $ multiplicar m n -- recorda que $ es associativa per la dreta
+
+-- Exercici reescriure amb mínim número de parentesis
+inc x = x + 1
+-- Inicial
+f1 x = inc (inc (inc x) + (inc x)) -- f1 x = 2x + 4
+f2 x = (+) (inc (inc x)) (inc (x + x))
+-- Reescrit amb la mateixa estructura: inc . inc x = inc . (inc x), l'aplicació te precedencia
+-- agrupant primer: (inc . inc) x = \x -> inc (inc x)
+f1' x = inc $ (inc . inc) x + inc x
+-- Sense parentesis, simplificar inc (inc (inc x + inc x)): Utilitzant inc (x + 1) = inc x + 1
+f1'' x = inc . inc $ inc x + inc x
