41dece271b3a0d2cafbe05d21d6e1d751e4eb29e
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-10-27 17:27:30 +0100

    Constructors feta

--- lab3/1-tipus.hs
+++ lab3/1-tipus.hs
@@ -1,4 +1,4 @@
--- Expressions de tipus
+-- 1.1 Expressions de tipus
 
 data Bit = O | I
   deriving (Show)
@@ -7,3 +7,15 @@ data Bit = O | I
 O .==. O = True
 I .==. I = True
 _ .==. _ = False
+
+-- 1.2 Constructors de valors paramètrics
+
+data Dbit = D Bit Bit
+  deriving (Show)
+
+bitAint :: Bit -> Int
+bitAint O = 0
+bitAint I = 1
+
+dbitAint :: Dbit -> Int
+dbitAint (D x y) = if x .==. O then bitAint y else 2 + bitAint y
