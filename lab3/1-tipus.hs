1aa5be3d91345cba135b235c40763b9b69dfebcc
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-10-27 17:52:36 +0100

    1.4 avançat

--- lab3/1-tipus.hs
+++ lab3/1-tipus.hs
@@ -22,3 +22,18 @@ dbitAint (D x y) = if x .==. O then bitAint y else 2 + bitAint y
 dbitAintP :: DBit -> Int
 dbitAintP (D O y) = bitAint y
 dbitAintP (D I y) = 2 + bitAint y
+
+-- 1.4 Definicions recursives
+data Nat = Zero | S Nat -- Cada S afegeix 1 al Nat, el succeeix
+  deriving (Show)
+
+natAint :: Nat -> Int -- Comptar els S fins que sigui Zero
+natAint Zero = 0
+natAint (S n) = 1 + natAint n
+
+sumaNat :: Nat -> Nat -> Nat -- Extraure S del primer Nat fins que sigui Zero (i tindres el Nat suma dels dos a la trucada recursiva)
+sumaNat Zero n = n
+sumaNat m Zero = m
+sumaNat (S m) n = S (sumaNat m n)
+
+data MInt = Pos Nat | Neg Nat | MZero
