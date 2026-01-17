5b84b93f695b8fba46fc7eb4c2fb74c0cbd3cee1
Elies <68759092+eliesgalvira@users.noreply.github.com>
2026-01-17 01:36:58 +0100

    Comentaris del profesor

---
+++ lab3/Lab3_1-corregit.hs
@@ -0,0 +1,166 @@
+module Lab3_1-corregit where
+
+-- 1.1 Expressions de tipus
+data Bit = O | I
+  deriving (Show, Eq)
+
+(.==.) :: Bit -> Bit -> Bool
+O .==. O = True
+I .==. I = True
+_ .==. _ = False
+
+-- 1.2 Constructors de valors paramètrics
+data DBit = D Bit Bit
+  deriving (Show, Eq)
+
+bitAint :: Bit -> Int
+bitAint O = 0
+bitAint I = 1
+
+dbitAint :: DBit -> Int
+dbitAint (D x y) = if x .==. O then bitAint y else 2 + bitAint y
+
+-- 1.3 Patrons
+dbitAintP :: DBit -> Int
+dbitAintP (D O y) = bitAint y
+dbitAintP (D I y) = 2 + bitAint y
+
+-- 1.4 Definicions recursives
+data Nat = Zero | S Nat -- Cada S afegeix 1 al Nat, el succeeix
+  deriving (Show, Eq)
+
+natAint :: Nat -> Int -- Comptar els S fins que sigui Zero
+natAint Zero = 0
+natAint (S n) = 1 + natAint n
+
+sumaNat :: Nat -> Nat -> Nat -- Extraure S del primer Nat fins que sigui Zero (i tindres el Nat suma dels dos a la trucada recursiva)
+sumaNat Zero n = n
+sumaNat m Zero = m
+sumaNat (S m) n = S (sumaNat m n)
+
+data MInt = Pos Nat | Neg Nat -- N'hi ha dos zeros, el Pos Zero i el Neg Zero
+  deriving (Show, Eq)
+
+intAnat :: Int -> Nat -- No contemplo el cas negatiu, per fer
+intAnat 0 = Zero
+intAnat n = S (intAnat (n - 1))
+
+mintAint :: MInt -> Int
+mintAint (Pos n) = natAint n
+mintAint (Neg n) = - natAint n
+
+intAmint :: Int -> MInt
+intAmint 0 = Pos Zero
+intAmint n = if n > 0 then Pos (intAnat n) else Neg (intAnat (-n))
+
+sumaMInt :: MInt -> MInt -> MInt
+sumaMInt (Pos m) (Pos n) = intAmint (natAint m + natAint n)
+sumaMInt (Neg m) (Neg n) = intAmint (-(natAint m + natAint n))
+sumaMInt (Pos m) (Neg n) = intAmint (natAint m - natAint n)
+sumaMInt (Neg m) (Pos n) = intAmint (natAint n - natAint m)
+
+-- Auxiliars per Nat (sense Int: per comparar i resta pura) -- es pot simplificar força!
+eqNat :: Nat -> Nat -> Bool
+eqNat Zero Zero = True
+eqNat (S m) (S n) = eqNat m n
+eqNat _ _ = False
+
+ltNat :: Nat -> Nat -> Bool  -- m < n
+ltNat Zero (S _) = True
+ltNat (S m) Zero = False
+ltNat (S m) (S n) = ltNat m n
+ltNat Zero Zero = False
+
+restaNat :: Nat -> Nat -> Nat  -- m - n, assumint m >= n (torna Zero si m < n, però nosaltres controlem)
+restaNat Zero _ = Zero
+restaNat m Zero = m
+restaNat (S m) (S n) = restaNat m n
+
+-- sumaMIntD: Sense conversions (només patrons, recursió i auxiliars)
+-- Zero implícit: Pos Zero + qualsevol = qualsevol, etc.
+sumaMIntD :: MInt -> MInt -> MInt
+-- Casos amb zero (Pos Zero)
+sumaMIntD (Pos Zero) n = n
+sumaMIntD m (Pos Zero) = m
+
+-- Pos + Pos
+sumaMIntD (Pos m) (Pos n) = Pos (sumaNat m n)
+
+-- Neg + Neg
+sumaMIntD (Neg m) (Neg n) = Neg (sumaNat m n)
+
+-- Pos + Neg: m - n (captura zero com Pos Zero)
+sumaMIntD (Pos m) (Neg n) =
+  if eqNat m n
+    then Pos Zero  -- m == n → 0
+    else if ltNat m n
+           then Neg (restaNat n m)  -- m < n → -(n - m)
+           else Pos (restaNat m n)  -- m > n → +(m - n)
+
+-- Neg + Pos: n - m (simètric, captura zero com Pos Zero)
+sumaMIntD (Neg m) (Pos n) =
+  if eqNat m n
+    then Pos Zero  -- m == n → 0
+    else if ltNat m n
+           then Pos (restaNat n m)  -- m < n → n - m > 0
+           else Neg (restaNat m n)  -- m > n → -(m - n) < 0
+
+-- Llista buida o conté un element i una llista (que pot ser buida)
+data Llista a = B | L a (Llista a)
+  deriving (Show, Eq)
+
+-- Conversió entre la nostra Llista i les llistes d'Haskell
+desdeL :: Llista a -> [a]
+desdeL B = []
+desdeL (L x xs) = x : desdeL xs
+
+aL :: [a] -> Llista a
+aL [] = B
+aL (x:xs) = L x (aL xs)
+
+-- Crea una llista amb els enters de n fins a 1
+initL :: Int -> Llista Int
+initL n
+  | n <= 0 = B
+  | otherwise = L n (initL (n - 1))
+
+-- Gira una llista (tail-recursiu amb acumulador)
+giraL :: Llista a -> Llista a
+giraL xs = rev xs B
+  where
+    rev :: Llista a -> Llista a -> Llista a
+    rev B acc = acc
+    rev (L y ys) acc = rev ys (L y acc)
+
+-- Llista de llistes: [[n..1],[n-1..1],..., [1]] amb el nostre tipus
+initLdL :: Int -> Llista (Llista Int)
+initLdL n
+  | n <= 0 = B
+  | otherwise = L (initL n) (initLdL (n - 1))
+
+-- Conversió d'una Llista de Llistes a llista d'Haskell
+desdeLdL :: Llista (Llista a) -> [[a]]
+desdeLdL B = []
+desdeLdL (L l ls) = desdeL l : desdeLdL ls
+
+-- Concatena dues Llistes
+concatenaL :: Llista a -> Llista a -> Llista a
+concatenaL B ys = ys
+concatenaL (L x xs) ys = L x (concatenaL xs ys)
+
+-- Aplana una Llista de Llistes en una sola Llista
+aplastaL :: Llista (Llista a) -> Llista a
+aplastaL B = B
+aplastaL (L l ls) = concatenaL l (aplastaL ls)
+
+-- Mapeig sobre Llistes
+mapejaL :: (a -> b) -> Llista a -> Llista b
+mapejaL _ B = B
+mapejaL f (L x xs) = L (f x) (mapejaL f xs)
+
+-- Versions no recursives utilitzant mapejaL
+initLdL2 :: Int -> Llista (Llista Int)
+initLdL2 n = mapejaL initL (aL [n, n-1 .. 1]) --- i sense [..]?
+
+desdeLdL2 :: Llista (Llista a) -> [[a]]
+desdeLdL2 lls = desdeL (mapejaL desdeL lls)
---
+++ lab4/Lab4-corregit.hs
@@ -0,0 +1,149 @@
+module Lab4-corregit where
+
+-- Importació necessària: utilitzem els tipus Llista i Nat definits al lab 3
+import Lab3_1
+import Data.Foldable
+import Data.List (partition, sort)
+
+-- ============================================================================
+-- Secció 1: Classes i instàncies
+-- ============================================================================
+
+-- Classe Iterador per a tipus contenidors de valors
+class Iterador t where
+    ele :: t a -> a  -- Retorna el primer element
+    seg :: t a -> t a  -- Elimina el primer element
+    hasnext :: t a -> Bool  -- False si buit, True altrament
+
+-- Instància de Iterador per Llista
+instance Iterador Llista where
+    ele (L x _) = x
+    seg (L _ xs) = xs
+    hasnext B = False
+    hasnext (L _ _) = True
+
+-- Funció sumElem usant l'iterador
+sumElem :: (Iterador t, Num a) => t a -> a
+sumElem contenedor
+    | hasnext contenedor = ele contenedor + sumElem (seg contenedor)
+    | otherwise = 0
+
+-- Instàncies de Eq, Ord i Enum per Nat (Eq ja estava derivat)
+instance Ord Nat where
+    compare Zero Zero = EQ
+    compare Zero (S _) = LT
+    compare (S _) Zero = GT
+    compare (S m) (S n) = compare m n
+
+instance Enum Nat where
+    toEnum 0 = Zero
+    toEnum n
+        | n > 0 = S (toEnum (n - 1))
+        | otherwise = error "Nat: toEnum with negative number"
+    
+    fromEnum Zero = 0
+    fromEnum (S n) = 1 + fromEnum n
+
+-- ============================================================================
+-- Secció 2: Les classes Semigroup i Monoid
+-- ============================================================================
+
+-- Instància de Semigroup per Nat
+instance Semigroup Nat where
+    (<>) = sumaNat
+
+-- Instància de Monoid per Nat
+instance Monoid Nat where
+    mempty = Zero
+
+-- ============================================================================
+-- Secció 3: La classe Foldable
+-- ============================================================================
+
+-- Instància de Foldable per Llista
+instance Foldable Llista where
+    foldMap f B = mempty
+    foldMap f (L x xs) = f x <> foldMap f xs
+    
+    foldr _ v B = v
+    foldr f v (L x xs) = f x (foldr f v xs)
+    
+    foldl _ v B = v
+    foldl f v (L x xs) = foldl f (f v x) xs
+
+-- Funció foldNat per a aplicar una funció recursivament sobre Nat
+foldNat :: (a -> a) -> a -> Nat -> a
+foldNat _ z Zero = z
+foldNat f z (S n) = f (foldNat f z n)
+
+-- Redefinir natAint i sumNat usant foldNat
+natAint' :: Nat -> Int
+natAint' = foldNat (+1) 0
+
+sumNat' :: Nat -> Nat -> Nat
+sumNat' m n = foldNat S n m
+
+-- ============================================================================
+-- Secció 3.1: Exercicis amb llistes
+-- ============================================================================
+
+-- Calcular el màxim d'una llista d'enters
+maxInt :: [Int] -> Int
+maxInt [] = error "maxInt: llista buida"
+maxInt xs = maximum xs
+
+-- Comptar el número de Nothings en una llista de Maybe
+countNothings :: [Maybe a] -> Int
+countNothings = length . filter (maybe True (const False)) -- i amb fold?
+
+-- Retornar una tupla amb l'element màxim i mínim
+maxMin :: [Int] -> (Int, Int)
+maxMin [] = error "maxMin: llista buida"
+maxMin xs = (maximum xs, minimum xs)
+
+-- Retornar una tupla amb el número d'elements i la seva suma
+countSum :: [Int] -> (Int, Int)
+countSum xs = (length xs, sum xs)
+
+-- Mitjana aritmètica
+mitjana :: [Int] -> Double
+mitjana [] = error "mitjana: llista buida"
+mitjana xs = fromIntegral (sum xs) / fromIntegral (length xs)
+
+-- Totes les aparicions del valor màxim
+totsMax :: [Int] -> [Int]
+totsMax [] = []
+totsMax xs = 
+    let maxVal = maximum xs -- i sense calcular el màxim?
+    in filter (== maxVal) xs
+
+-- Posicions on apareix un caràcter en un String
+-- Les posicions comencen des de 0 però l'exemple mostra [1,3,5] per "patata"
+-- que correspon a les posicions 0-indexed: 1, 3, 5
+posicionsLletra :: Char -> String -> [Int]
+posicionsLletra c str = 
+    let indexed = zip [0..] str -- i sense zip?
+    in map fst (filter ((== c) . snd) indexed)
+
+-- Suma de cada subllista
+sumPar :: [[Int]] -> [Int]
+sumPar = map sum
+
+-- Suma total de tots els elements
+sumTot :: [[Int]] -> Int
+sumTot = sum . concat
+
+-- Separar una llista segons un valor
+-- Retorna una tupla amb els valors menors o iguals i la resta
+separar :: Ord a => [a] -> a -> ([a], [a])
+separar xs pivot = 
+    let (menors, majors) = partition (<= pivot) xs
+    in (sort menors, sort majors) -- cal ordenar?
+
+-- Ordenar una llista (quicksort)
+ordenar :: Ord a => [a] -> [a]
+ordenar [] = []
+ordenar (x:xs) = 
+    let (menors, majors) = partition (< x) xs -- pensat per fer servir separar...
+    in ordenar menors ++ [x] ++ ordenar majors
+
